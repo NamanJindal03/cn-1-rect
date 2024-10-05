31e4412c629dd7b0e47722252cd1efc5b355123a
Naman Jindal <namanjindal113@gmail.com>
2024-10-05 22:32:51 +0530

    playing with useEffect and setIntervals and setTimeout

--- src/lifecycle_functional/Child2.js
+++ src/lifecycle_functional/Child2.js
@@ -1,16 +1,45 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+
+let timerId;
 
 export default function Child2() {
-    useEffect(()=>{
-        console.log('mounting of child2');
 
-        //unmounting of child2 
-        return ()=>{
-            console.log('unmounting of child 2')
+    const [count, setCounter] = useState(Math.floor(Math.random()*10));
+    // useEffect(() => {
+    //     let intervalId = setInterval(() => {
+    //         console.log(count)
+    //         // count++;
+    //         // console.log(count++);
+    //         setCounter(count + 1)
+    //     }, 1000);
+    //     return () => clearInterval(intervalId);
+    // }, [count])
+
+    /* 
+        setCounter((state)=>{
+            return state + 1;
+        })
+    */
+
+    useEffect(() => {
+        timerId = setTimeout(() => {
+            console.log(count)
+            // count++;
+            // console.log(count++);
+            setCounter(count + 1)
+        }, 1000);
+    }, [count])
+
+    useEffect(()=>{
+        return () => {
+            console.log('clearing out the timer', timerId)
+            clearTimeout(timerId)
         }
-    })
+    },[])
   return (
 
-    <div>Child2</div>
+    <div>Child2: {count}</div>
   )
 }
+
+
