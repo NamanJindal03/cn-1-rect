ae81f0620b4a7ded63e25ee1f196514f6320ad3c
Naman Jindal <namanjindal113@gmail.com>
2024-10-08 22:57:08 +0530

    class based

--- src/classBased/App.js
+++ src/classBased/App.js
@@ -10,19 +10,34 @@ import Child from './Child'
 export default class Car extends Component{
     constructor(){
         super()
+        this.state = {}
         console.log('first')
     }
 
+    async getTodo(){
+        const jsonData = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+        const data = await jsonData.json();
+        this.setState(data)
+    }
     componentDidMount(){
-        console.log('third')
+        this.getTodo();
+    }
+
+    componentDidUpdate(){
+        //we have a targetted function, this does not get executed atht eht eim fo mount
     }
 
     render(){
         return (
             <>
                 {console.log('second')}
+
+                {this.state.userId}
+                {this.state.id}
+                {this.state.title}
                 <div>Class Based App Component</div>
                 <Child title={'abcd'}/>
+
             </>
 
         )
