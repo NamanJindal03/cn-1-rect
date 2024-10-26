91fcf91ee631429cd9d0a13190bed3e86f9ec87d
Naman Jindal <namanjindal113@gmail.com>
2024-10-26 22:09:34 +0530

    handled realtime get, create

--- src/blogMiniProject/App.js
+++ src/blogMiniProject/App.js
@@ -2,21 +2,61 @@ import React, { useState, useEffect } from 'react'
 import BlogDisplay from './components/BlogDisplay'
 import BlogForm from './components/BlogForm'
 import { db } from './db/firebaseInit';
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, doc, setDoc, getDocs, onSnapshot, query, orderBy  } from "firebase/firestore"; 
 
 export default function App() {
     const [blogs, setBlogs] = useState([]);
 
-    useEffect(()=>{
-        console.log(db)
-    },[])
+    const getBlogsFromDb = async() => {
+        const blogFromDb = [];
+        const querySnapshot = await getDocs(collection(db, "mini-blog"));
+        // console.log(querySnapshot)
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const localBlog = {
+                content: data.content,
+                title: data.title,
+                id: doc.id
+            }
+            blogFromDb.push(localBlog)
+        });
+        setBlogs(blogFromDb)
+    }
 
+    useEffect(()=>{
+        // getBlogsFromDb()
+        // const unsub = onSnapshot(
+        //     doc(db, "mini-blog"),
+        //     (doc) => {
+        //       console.log(doc)
+        //     }
+        // );
+        const q = query(collection(db, "mini-blog"), orderBy("title", "asc"));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const blogFromDb = [];
+            querySnapshot.forEach((doc) => {
+                blogFromDb.push({...doc.data(), id: doc.id});
+            });
+            setBlogs(blogFromDb)
+            console.log(blogFromDb)
+        });
+
+    },[])
+    //TODO: way1
+    // const addBlogInDb = async (title, content) => {
+    //     const docRef = await addDoc(collection(db, "mini-blog"), {
+    //         title: title,
+    //         content: content
+    //       });
+    //       console.log("Document written: ", docRef);
+    // }
+
+    //TODO: way2
     const addBlogInDb = async (title, content) => {
-        const docRef = await addDoc(collection(db, "mini-blog"), {
-            title: title,
-            content: content
-          });
-          console.log("Document written: ", docRef);
+        const docRef = doc(collection(db, "mini-blog"));
+        console.log(docRef);
+        await setDoc(docRef, {title, content});
+        // console.log("Document written: ", docRef);
     }
 
     const addBlog = ({title, content, id}) => {
