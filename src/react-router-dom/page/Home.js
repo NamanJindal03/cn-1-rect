aab1ae40370397fe628dae41f09c78c55bae756c
Naman Jindal <namanjindal113@gmail.com>
2024-11-09 22:59:17 +0530

    search param

--- src/react-router-dom/page/Home.js
+++ src/react-router-dom/page/Home.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useSearchParams, createSearchParams } from 'react-router-dom'
 export default function Home() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    // console.log(useLocation());
+    const citySearchparam = searchParams.get('city');
   return (
     <>
-        
-        <div>This is home</div>
-        
+        {
+            ['delhi', 'punjab', 'up', 'himachal'].map((value, index)=>{
+                return (
+                    <div key={index}>
+                        <input 
+                            type="radio" 
+                            id={value}
+                            checked={value==citySearchparam}
+                            value={value}
+                            onChange={(e)=>{
+                                setSearchParams(createSearchParams({city: value}))
+                            }}
+                        />
+                        <label htmlFor={value}>{value}</label>
+                    </div>
+                )
+            })
+        }
     </>
   )
 }
--- src/react-router-dom/page/NotFound.js
+++ src/react-router-dom/page/NotFound.js
@@ -1,6 +1,12 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 export default function NotFound() {
+    const navigate = useNavigate();
+    useEffect(()=>{
+        setTimeout(()=>{
+            navigate('/home', {state: {comingFromRedirection: true}})
+        },5000)
+    },[])
   return (
     <div>NotFound</div>
   )
