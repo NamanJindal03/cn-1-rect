e333b695d1daaeb5215f92c2f3c8ebe90afaab15
Naman Jindal <namanjindal113@gmail.com>
2024-11-14 23:04:36 +0530

    handled partial filter

--- src/busyBuy/pages/Home.js
+++ src/busyBuy/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import {getDocs, collection} from "firebase/firestore"
 import { db } from '../db/firebaseInit'
+const allCategories = ['men', 'women', 'kitchen', 'electronics'];
 export default function Home() {
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [priceFilterState, setPriceFilterState] = useState(75000);
+    const [categoryFilterStates, setCategoryFilterStates] = useState({});
 
     useEffect(()=>{
         (async function(){
@@ -15,20 +17,32 @@ export default function Home() {
             })
             setProducts([...localProducts]);
             setFilteredProducts([...localProducts]);
-        })()
+        })();
+        const localCategoryBuilder={};
+        for(let i=0; i<allCategories.length;i++){
+            localCategoryBuilder[allCategories[i]] = false;
+        }
+        setCategoryFilterStates(localCategoryBuilder) 
     },[])
     function filterProducts(filteredPrice){
         const newFilteredProducts = products.filter((product)=>{
             if(product.price <= filteredPrice){
                 return true;
             }
+            //another check -> includes -> 
         })
         console.log(newFilteredProducts)
-        setFilteredProducts([...newFilteredProducts])
+        setFilteredProducts([...newFilteredProducts]) 
     }
     function handlePriceFilter(e){
         setPriceFilterState(e.target.value);
         filterProducts(e.target.value);
+    }
+    function handleCategoryFilter(e){
+        setCategoryFilterStates((prev)=>{
+            return {...prev, [e.target.name]: e.target.checked} //can cause issue
+        })
+        filterProducts(priceFilterState);
     }
   return (
     <>
@@ -42,7 +56,20 @@ export default function Home() {
                 onChange={handlePriceFilter}
                 // onMouseUp={()=>filterProducts()}
             />
-             
+            <div>Category</div>
+            {
+                Object.keys(categoryFilterStates).map((categoryName)=>{
+                    return(<>
+                        <input 
+                            type="checkbox" 
+                            checked={categoryFilterStates[categoryName]} 
+                            name={categoryName}
+                            onChange={handleCategoryFilter}
+                        />
+                        <span>{categoryName}</span>
+                    </>)
+                })
+            }
         </div>
         <div className='productContainer'>
             {filteredProducts.map((product)=>{
