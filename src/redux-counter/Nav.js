df76db79f41ec318067daf0e2b00b91c06d19e89
Naman Jindal <namanjindal113@gmail.com>
2024-11-21 22:45:08 +0530

    cart integrated 100%

--- src/redux-counter/Nav.js
+++ src/redux-counter/Nav.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from './ThemeSlice';
 export default function Nav() {
-  const theme = useSelector((store) => store.theme)
+  const theme = useSelector((store) => store.theme);
+  const cartData = useSelector((store)=> store.cart);
   const dispatch = useDispatch();
   return (
     <nav style={{backgroundColor: 'black', padding: "2px"}}>
         {/* number of items in the cart */}
         <div style={{color: 'white'}}>
-            CART: 
+            CART: {cartData.length}
         </div>
         <button 
             onClick={() => dispatch(toggleTheme())}
